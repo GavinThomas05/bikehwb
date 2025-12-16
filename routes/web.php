3a7c3644974ed42a9efc3717d6361b86aa58ad76
GavinThomas05 <gavinlthomas05@gmail.com>
2025-12-16 19:43:26 +0000

    deleted redundant file and removed redundant routes in web

--- routes/web.php
+++ routes/web.php
@@ -8,26 +8,6 @@ Route::get('/', function () {
     return view('home');
 });
 
-// Route 1: returns a welcome message
-Route::get('/post', function () {
-    return view('addpost');
-});
-
-// Route 2: returns a different message
-Route::get('/goodbye', function () {
-    return 'Goodbye, world!';
-});
-
-// Route 3: redirects to welcome  message
-Route::get('/redirect', function () {
-    return redirect('/hello');
-});
-
-//accepts a string and outputs in browser
-Route::get('/show/{text}', function ($text) {
-    return "You entered: " . $text;
-    });
-
 // Route to display all posts on the homepage
 Route::get('/', [PostController::class, 'index'])->name('home');
 
